3a1e73a404889503a5ab33e74a095f44172a04fb
Alexander Savinykh <Alexander.Savinykh@hoerburger.de>
2019-01-16 17:41:32 +0100

    adding processStats

--- src/processStats.ts
+++ src/processStats.ts
@@ -4,7 +4,14 @@ const windowsCommand = 'wmic PROCESS GET name,PrivatePageCount /format:rawxml'
 const windowsPageSize = 0x1000
 
 export class ProcessStats {
-    read() {
-        child_process.exec('wimc PROCESS')
+    read(): Promise<any> {
+        return new Promise((resolve, reject) => {
+            child_process.exec(windowsCommand, (error, stdout, stderr) => {
+                if (error)
+                    reject(error);
+                const parser = new DOMParser();
+                const xmlDoc = parser.parseFromString(stdout, "text/xml");    
+            })
+        });
     }
 }
