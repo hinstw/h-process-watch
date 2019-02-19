181912053dab99dd3a2657bf0f5e66d90d5ec506
Alexander Savinykh <Alexander.Savinykh@hoerburger.de>
2019-02-19 18:24:40 +0100

    check processes periodicly

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,13 @@
 import { ProcessStats } from "./processStats";
 
 export class App {
-    processStats: ProcessStats;
     run() {
         console.log('Now running app...');
-        this.processStats = new ProcessStats();
         setInterval(() => this.tickCheck(), 5000);
     }
     async tickCheck() {
-        await this.processStats.read();
+        const processStats = new ProcessStats();
+        await processStats.read();
     }
     throw() {
         throw new Error('Test');
